3575c8d9f090e1b5293368a455cc5c95f84fc21f
Ian Kennedy <exposure@gmail.com>
2016-05-14 13:31:49 -0400

    Footer, buttons, smooth-scroll, sass clean-up

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
+  // smooth scrolling
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
 
-    // google-analytics
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    var target = this.hash;
+    var $target = $(target);
 
-    ga('create', 'UA-77429513-1', 'auto');
-    ga('send', 'pageview');
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
+
+  // google-analytics
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+  ga('create', 'UA-77429513-1', 'auto');
+  ga('send', 'pageview');
 
 });
